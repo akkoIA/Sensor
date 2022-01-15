91af98ca6c11eef95f15c752f754c057c27915e0
ishikawatatuya <shichuanzi83@gmail.com>
2022-01-15 20:03:32 +0900

    service途中

--- app/src/main/java/com/example/sensor/MainActivity.kt
+++ app/src/main/java/com/example/sensor/MainActivity.kt
@@ -15,28 +15,28 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.absoluteValue
 import kotlin.properties.Delegates
 
-class MainActivity : AppCompatActivity(),SensorEventListener{
-    private var mManager: SensorManager by Delegates.notNull<SensorManager>()
-    private var mSensor: Sensor by Delegates.notNull<Sensor>()
+class MainActivity : AppCompatActivity(){
+//    private var mManager: SensorManager by Delegates.notNull<SensorManager>()
+//    private var mSensor: Sensor by Delegates.notNull<Sensor>()
 
     var isStart = false
     var isWorking = false
 
-    var defaultX = 0f
-    var defaultY = 0f
-    var defaultZ = 0f
-
-    var sound1: MediaPlayer? = null
-    var sound2: MediaPlayer? = null
-    var sound3: MediaPlayer? = null
+//    var defaultX = 0f
+//    var defaultY = 0f
+//    var defaultZ = 0f
+//
+//    var sound1: MediaPlayer? = null
+//    var sound2: MediaPlayer? = null
+//    var sound3: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sound1=MediaPlayer.create(this,R.raw.doamusic)
-        sound2=MediaPlayer.create(this,R.raw.siren)
-        sound3=MediaPlayer.create(this,R.raw.god)
+//        sound1=MediaPlayer.create(this,R.raw.doamusic)
+//        sound2=MediaPlayer.create(this,R.raw.siren)
+//        sound3=MediaPlayer.create(this,R.raw.god)
 
         val text2=findViewById<TextView>(R.id.timer)
 
@@ -58,7 +58,8 @@ class MainActivity : AppCompatActivity(),SensorEventListener{
                 buttonStop.visibility=View.VISIBLE
                 buttonStop.isClickable=true
 
-                isStart = true
+                val intent = Intent(applicationContext, SoundService::class.java)
+                startForegroundService(intent)
 
             }
         }
@@ -76,12 +77,15 @@ class MainActivity : AppCompatActivity(),SensorEventListener{
 
             isWorking=false
             text2.text="待機中"
+
+            val intent = Intent(applicationContext, SoundService::class.java)
+            stopService(intent)
         }
 
-
-        mManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        //その他のセンサーを取得する場合には引数を違うものに変更する
-        mSensor = mManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+//
+//        mManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+//        //その他のセンサーを取得する場合には引数を違うものに変更する
+//        mSensor = mManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 //
 //
 //
@@ -95,50 +99,50 @@ class MainActivity : AppCompatActivity(),SensorEventListener{
 //        sensorManager.registerListener()
     }
 
-    override fun onSensorChanged(event: SensorEvent?) {
-        if(isStart){
-            defaultX = event?.values?.get(0)?.absoluteValue ?: 0f
-            defaultY = event?.values?.get(1)?.absoluteValue ?: 0f
-            defaultZ = event?.values?.get(2)?.absoluteValue ?: 0f
+//    override fun onSensorChanged(event: SensorEvent?) {
+//        if(isStart){
+//            defaultX = event?.values?.get(0)?.absoluteValue ?: 0f
+//            defaultY = event?.values?.get(1)?.absoluteValue ?: 0f
+//            defaultZ = event?.values?.get(2)?.absoluteValue ?: 0f
+//
+//            isStart = false
+//            isWorking=true
+//        }
+//
+//        if(isWorking){
+//            //defaultNと現在値の差分を取る
+//            if(event?.values == null) return
+//            val judX = event.values.get(0).absoluteValue - defaultX > 1
+//            val judY = event.values.get(1).absoluteValue - defaultY > 1
+//            val judZ = event.values.get(2).absoluteValue - defaultZ > 1
+//
+//            if(judX || judY || judZ){
+//                //音を鳴らす
+//                val sharedPref = getSharedPreferences("sound", Context.MODE_PRIVATE)
+//                val sound=sharedPref.getString("sound","sound1")
+//
+//                when(sound){
+//                    "sound1" -> sound1?.start()
+//                    "sound2" -> sound2?.start()
+//                    "sound3" -> sound3?.start()
+//                    else -> sound1?.start()
+//                }
+//            }
+//
+//        }
 
-            isStart = false
-            isWorking=true
-        }
+//    }
 
-        if(isWorking){
-            //defaultNと現在値の差分を取る
-            if(event?.values == null) return
-            val judX = event.values.get(0).absoluteValue - defaultX > 1
-            val judY = event.values.get(1).absoluteValue - defaultY > 1
-            val judZ = event.values.get(2).absoluteValue - defaultZ > 1
-
-            if(judX || judY || judZ){
-                //音を鳴らす
-                val sharedPref = getSharedPreferences("sound", Context.MODE_PRIVATE)
-                val sound=sharedPref.getString("sound","sound1")
-
-                when(sound){
-                    "sound1" -> sound1?.start()
-                    "sound2" -> sound2?.start()
-                    "sound3" -> sound3?.start()
-                    else -> sound1?.start()
-                }
-            }
-
-        }
-
-    }
-
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-    }
+//    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+//    }
 
     override fun onPause() {
         super.onPause()
-        mManager.unregisterListener(this)
+//        mManager.unregisterListener(this)
     }
 
     override fun onResume() {
         super.onResume()
-        mManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_UI)
+//        mManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_UI)
     }
 }
---
+++ app/src/main/java/com/example/sensor/SoundService.kt
@@ -0,0 +1,98 @@
+package com.example.sensor
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.media.MediaPlayer
+import android.os.CountDownTimer
+import android.os.IBinder
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.absoluteValue
+import kotlin.properties.Delegates
+
+class SoundService : Service(), SensorEventListener {
+    private var mManager: SensorManager by Delegates.notNull<SensorManager>()
+    private var mSensor: Sensor by Delegates.notNull<Sensor>()
+
+    var isStart = false
+    var isWorking = false
+
+    var defaultX = 0f
+    var defaultY = 0f
+    var defaultZ = 0f
+
+    var sound1: MediaPlayer? = null
+    var sound2: MediaPlayer? = null
+    var sound3: MediaPlayer? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        sound1=MediaPlayer.create(this,R.raw.doamusic)
+        sound2=MediaPlayer.create(this,R.raw.siren)
+        sound3=MediaPlayer.create(this,R.raw.god)
+
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        mManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        //その他のセンサーを取得する場合には引数を違うものに変更する
+        mSensor = mManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+        isStart = true
+
+        return START_STICKY
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        startForeground()
+
+        if(isStart){
+            defaultX = event?.values?.get(0)?.absoluteValue ?: 0f
+            defaultY = event?.values?.get(1)?.absoluteValue ?: 0f
+            defaultZ = event?.values?.get(2)?.absoluteValue ?: 0f
+
+            isStart = false
+            isWorking=true
+        }
+
+        if(isWorking){
+            //defaultNと現在値の差分を取る
+            if(event?.values == null) return
+            val judX = event.values.get(0).absoluteValue - defaultX > 1
+            val judY = event.values.get(1).absoluteValue - defaultY > 1
+            val judZ = event.values.get(2).absoluteValue - defaultZ > 1
+
+            if(judX || judY || judZ){
+                //音を鳴らす
+                val sharedPref = getSharedPreferences("sound", Context.MODE_PRIVATE)
+                val sound=sharedPref.getString("sound","sound1")
+
+                when(sound){
+                    "sound1" -> sound1?.start()
+                    "sound2" -> sound2?.start()
+                    "sound3" -> sound3?.start()
+                    else -> sound1?.start()
+                }
+            }
+
+        }
+
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mManager.unregisterListener(this)
+    }
+}
